ab1fb7273a24ebd4753d862157110671440df94c
MobDe <MobDevX.outlook.com>
2022-02-17 13:57:23 +0700

    Model Photo Response

---
+++ app/src/main/java/com/example/photogallery/model/Photo.kt
@@ -0,0 +1,7 @@
+package com.example.photogallery.model
+
+data class Photo(
+    var title: String = "",
+    var id: String = "",
+    var url_s: String = ""
+)
---
+++ app/src/main/java/com/example/photogallery/model/PhotoResponse.kt
@@ -0,0 +1,7 @@
+package com.example.photogallery.model
+
+import com.google.gson.annotations.SerializedName
+
+class PhotoResponse {
+    lateinit var photos: List<Photo>
+}
