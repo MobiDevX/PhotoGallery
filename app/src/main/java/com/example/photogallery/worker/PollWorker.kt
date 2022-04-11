167867352f4c2501f74c2787f58856dca1702c08
MobDe <MobDevX.outlook.com>
2022-04-11 21:19:00 +0700

    Change PollWorker & FlickrFetchr

--- app/src/main/java/com/example/photogallery/worker/PollWorker.kt
+++ app/src/main/java/com/example/photogallery/worker/PollWorker.kt
@@ -4,13 +4,43 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.photogallery.FlickrFetchr
+import com.example.photogallery.QueryPreferences
+import com.example.photogallery.model.GalleryItem
 
 private const val TAG = "PollWorker"
 
 class PollWorker(val context: Context, workerParameters: WorkerParameters)
     : Worker(context, workerParameters) {
     override fun doWork(): Result {
-        Log.i(TAG, "Work request triggered")
+        //Log.i(TAG, "Work request triggered")
+        val query = QueryPreferences.getStoredQuery(context)
+        val lastResultId = QueryPreferences.getLastResultId(context)
+        val items: List<GalleryItem> = if (query.isEmpty()) {
+            FlickrFetchr().fetchPhotosRequest()
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } else {
+            FlickrFetchr().searchPhotosRequest(query)
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } ?: emptyList()
+
+        if (items.isEmpty()) {
+            return Result.success()
+        }
+        val resultId = items.first().id
+        if (resultId == lastResultId) {
+            Log.i(TAG, "Got an old result: $resultId")
+        } else {
+            Log.i(TAG, "Got a new result: $resultId")
+            QueryPreferences.setLastResultId(context, resultId)
+        }
+
         return Result.success()
     }
 }
